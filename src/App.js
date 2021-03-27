705c4ef9bfb36f77eada2352d21564fb74ac35e7
unknown <santoserick@gmail.com>
2021-03-27 20:30:11 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
 
 function App() {
-  return <div>React App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+function Footer() {
+  return <div className={styles.footer}>FOOTER</div>;
+}
+
+export default Footer;
