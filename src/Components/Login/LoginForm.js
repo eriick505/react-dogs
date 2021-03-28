f9481e884b54700479c0b9225448398d5e4ce5d1
unknown <santoserick@gmail.com>
2021-03-27 22:08:22 -0300

    Hook useForm criado e adicionado ao componente input para tornar os valores do input reativo e para validação dos tipos de input

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,39 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../Forms/Input/Input';
 import Button from '../Forms/Button/Button';
+import useForm from '../../Hooks/useForm';
 
 function LoginForm() {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm();
+  const password = useForm();
+  console.log(username);
+  console.log(password);
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    })
-      .then(response => {
-        console.log(response);
-        return response.json();
+    const isFieldsFullFill = username.validate() && password.validate();
+
+    if (isFieldsFullFill) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
       })
-      .then(json => {
-        console.log(json);
-        return json;
-      });
+        .then(response => {
+          console.log(response);
+          return response.json();
+        })
+        .then(json => {
+          console.log(json);
+          return json;
+        });
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuario" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuario" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
