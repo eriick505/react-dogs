3725c5d47fdcf556c59ecb09a19c4dfd20f38abf
Erick <santoserick9@gmail.com>
2021-04-06 20:08:34 -0300

    mudança no diretorio do componente UserPhotoPost e criação do arquivo de estilo

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import UserHeader from "./UserHeader/UserHeader";
-import UserPhotoPost from "./UserPhotoPost";
+import UserPhotoPost from "./UserPhotoPost/UserPhotoPost";
 import UserStats from "./UserStats";
 import Feed from "../Feed/Feed";
 
