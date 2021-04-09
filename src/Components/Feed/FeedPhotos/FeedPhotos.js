97eb101a76caf86fb01ccc6708cdeaab66171e52
unknown <santoserick@gmail.com>
2021-04-09 19:02:25 -0300

    remoção da desestruturação do json na função de request

--- src/Components/Feed/FeedPhotos/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos/FeedPhotos.js
@@ -11,8 +11,8 @@ function FeedPhotos({ setModalPhoto }) {
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
+      const { url, options } = PHOTOS_GET({ page: 1, total: 8, user: 0 });
+      request(url, options);
     }
     fetchPhotos();
   }, [request]);
