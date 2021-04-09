b0db38769259c5e3af378e9a1059d0ea4508acc6
unknown <santoserick@gmail.com>
2021-04-09 17:55:57 -0300

    Criado o arquivo de estilo para o componente feedModal, criado um efeito para fazer sempre uma requisição a foto passada por props e o retorno dessa requisição seja passado para o componente de conteudo de foto, adicionado condicional para verificar se a requisição retornou erro ou esta carregando

--- src/Components/Feed/FeedModal/FeedModal.js
+++ src/Components/Feed/FeedModal/FeedModal.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import useFetch from '../../../Hooks/useFetch';
+import { PHOTO_GET } from '../../../api';
+import Error from '../../Helper/Error/Error';
+import Loading from '../../Helper/Loading/Loading';
+import PhotoContent from '../../Photo/PhotoContent/PhotoContent';
+import styles from './FeedModal.module.css';
 
-function FeedModal() {
-  return <div>FEED MODAL</div>;
+function FeedModal({ photo }) {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    const { response, json } = request(url, options);
+  }, [photo, request]);
+
+  return (
+    <div className="styles.modal">
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 }
 
 export default FeedModal;
