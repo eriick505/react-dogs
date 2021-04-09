5fea5d70bb3a74cb0c452d5c0693f65f304cd9a9
unknown <santoserick@gmail.com>
2021-04-09 18:05:39 -0300

    Criado os componentes de Photo que recebe o componente criado PhotoContent que recebe os dados da foto clicada no componente FeedPhotosItem, criado o arquivo de estilo do componente PhotoContent, desestruturado os dados recebido pelas props no componente PhotoContent e listado os dados no compontente

---
+++ src/Components/Photo/Photo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Photo() {
+  return <div>photo</div>;
+}
+
+export default Photo;
---
+++ src/Components/Photo/PhotoComments/PhotoComments.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PhotoComments() {
+  return <div>comentarios</div>;
+}
+
+export default PhotoComments;
