3bae3895809cd12eb58d57eee0d5a079304f3d99
unknown <santoserick@gmail.com>
2021-04-12 19:55:16 -0300

    criada uma listagem dos comentarios recebidos pelas props e adicionada uma condicional para exibir o formulario de comentarios apenas se existir usuario autenticado

--- src/Components/Photo/PhotoComments/PhotoComments.js
+++ src/Components/Photo/PhotoComments/PhotoComments.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { UserContext } from '../../../Contexts/UserContext';
+import PhotoCommentsForm from '../PhotoCommentsForm/PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-function PhotoComments() {
-  return <div>comentarios</div>;
+function PhotoComments(props) {
+  const { login } = React.useContext(UserContext);
+  const [comments, setComments] = React.useState(() => props.comments);
+
+  return (
+    <>
+      <ul className={styles.comment}>
+        {comments.map(comment => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>{' '}
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 }
 
 export default PhotoComments;
