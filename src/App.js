73b87ef8309a6acbb46c4f8d888862afe0b0969f
unknown <santoserick@gmail.com>
2021-04-21 20:57:03 -0300

    odernação na importação dos componentes e funções, criada uma nova rota para a pagina single das fotos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
-import "./App.css";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Components/Login/Login";
-import { UserStorage } from "./Contexts/UserContext";
-import User from "./Components/User/User";
-import ProtectedRouter from "./Components/Helper/ProtectedRouter/ProtectedRouter";
+import React from 'react';
+import './App.css';
+
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import User from './Components/User/User';
+import Photo from './Components/Photo/Photo';
+import ProtectedRouter from './Components/Helper/ProtectedRouter/ProtectedRouter';
+
+import { UserStorage } from './Contexts/UserContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -19,6 +22,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
             <ProtectedRouter path="conta/*" element={<User />} />
+            <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>
