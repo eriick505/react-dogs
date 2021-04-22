cdebfdc70e17431d63c0d59d1d256bea00dce84a
unknown <santoserick@gmail.com>
2021-04-21 21:01:31 -0300

    Criação da estrutura da pagina Single, ao carregar o componente é feito uma requisição ao endpoint que retorna os dados da foto com base no id passado que foi desestruturado do hook params do router doom

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+import PhotoContent from './PhotoContent/PhotoContent';
+import Error from '../Helper/Error/Error';
+import Loading from '../Helper/Loading/Loading';
+
+import useFetch from '../../Hooks/useFetch';
+import { useParams } from 'react-router-dom';
+import { PHOTO_GET } from '../../api';
+
 function Photo() {
-  return <div>photo</div>;
+  const { id } = useParams();
+
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url, options);
+  }, [request, id]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className="container mainContainer">
+        <PhotoContent data={data} single={true} />
+      </section>
+    );
+  else return null;
 }
 
 export default Photo;
