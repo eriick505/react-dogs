57b3687e1a0ff587b194260119c969d3e59b1883
unknown <santoserick@gmail.com>
2021-04-21 21:13:04 -0300

    Adicionado verificação se caso o estado de infinite seja false exibe uma mensagem que não existe mais postagens

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -47,6 +47,17 @@ function Feed({ user }) {
           setInfinite={setInfinite}
         />
       ))}
+      {!infinite && !user && (
+        <p
+          style={{
+            textAlign: 'center',
+            padding: '2rem 0 4rem 0',
+            color: '#888',
+          }}
+        >
+          Não existem mais postagens.
+        </p>
+      )}
     </div>
   );
 }
