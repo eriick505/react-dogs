63654a9de44051359b9a27e2f91b5f9ba3f728c3
Erick <santoserick9@gmail.com>
2021-04-25 19:20:06 -0300

    registrada a rota dinamica do perfil do usuario e importação do componente que a rota do perfil leva

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
-import User from './Components/User/User';
-import Photo from './Components/Photo/Photo';
-import ProtectedRouter from './Components/Helper/ProtectedRouter/ProtectedRouter';
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
+import UserProfile from "./Components/User/UserProfile/UserProfile";
+import Photo from "./Components/Photo/Photo";
+import ProtectedRouter from "./Components/Helper/ProtectedRouter/ProtectedRouter";
 
-import { UserStorage } from './Contexts/UserContext';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserStorage } from "./Contexts/UserContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRouter path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
