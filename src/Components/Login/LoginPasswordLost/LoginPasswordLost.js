4e03ac3c40ce670789d66bff5d219ca666373e0c
Erick <santoserick9@gmail.com>
2021-04-26 22:05:10 -0300

    criada as estrutura das paginas de reset e solicitação de senha perdida, criada função que faz a requisição para as rotas necessarias para reset da senha

--- src/Components/Login/LoginPasswordLost/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost/LoginPasswordLost.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React from "react";
+
+import Input from "../../Forms/Input/Input";
+import Button from "../../Forms/Button/Button";
+import Error from "../../Helper/Error/Error";
+
+import useForm from "../../../Hooks/useForm";
+import useFetch from "../../../Hooks/useFetch";
+
+import { PASSWORD_LOST } from "../../../api";
 
 function LoginPasswordLost() {
-  return <div>LoginPasswordLost</div>;
+  const login = useForm();
+
+  const { loading, error, request, data } = useFetch();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace("perdeu", "resetar"),
+      });
+      request(url, options);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a Senha?</h1>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+      <Error error={error} />
+    </section>
+  );
 }
 
 export default LoginPasswordLost;
