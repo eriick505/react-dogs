fe61d77c9503b0d4fcf74957abf2e73ccc44bc8b
Erick <santoserick9@gmail.com>
2021-04-28 23:00:59 -0300

    adicionado o react lazy + suspense para evitar o carregamento da biblioteca victory em todas as outras paginas que não necessitam do pacote de graficos

--- src/Components/User/UserStats/UserStats.js
+++ src/Components/User/UserStats/UserStats.js
@@ -3,10 +3,13 @@ import React from "react";
 import Head from "../../Head/Head";
 import Loading from "../../Helper/Loading/Loading";
 import Error from "../../Helper/Error/Error";
-import UserStatsGraphs from "../UserStatsGraphs/UserStatsGraphs";
 
 import { STATS_GET } from "../../../api";
 import useFetch from "../../../Hooks/useFetch";
+
+const UserStatsGraphs = React.lazy(() =>
+  import("../UserStatsGraphs/UserStatsGraphs")
+);
 
 function UserStats() {
   const { data, error, loading, request } = useFetch();
@@ -23,10 +26,10 @@ function UserStats() {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 }
